3c0c63fcc09fe77093da6316a24ea5097620a3fb
David MARTIN <martidav@iut-acy.local>
2019-09-30 10:44:07 +0200

    Test fonction max_value

--- test_S2.py
+++ test_S2.py
@@ -8,3 +8,7 @@ Created on Mon Sep 30 09:30:39 2019
 import S1_algotools
 import pytest
 
+def test_max_value_working1():
+	tab_list=[1,2,3,4,5,6,7,8,9]
+	test, valeur_max=S1_algotools.average_above_zero(tab_list)
+	assert test==9
