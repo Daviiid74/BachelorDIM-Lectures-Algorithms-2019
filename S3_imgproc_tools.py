f9f7ac64edcae1d5e0aa317ef01038c0edc36756
David MARTIN <martidav@iut-acy.local>
2019-09-30 14:46:11 +0200

    Ajout d'une image et inversion d'image S3_imgproc_tools

--- S3_imgproc_tools.py
+++ S3_imgproc_tools.py
@@ -5,3 +5,21 @@ Created on Mon Sep 30 13:32:48 2019
 @author: David
 """
 
+import cv2
+
+img = cv2.imread('landscape.png')
+
+cv2.imshow('input', img)
+cv2.waitkey()
+
+print('image shape', img.shape)
+
+'''for row in range(img.shape[2]):
+    for col in range(img.shape[1]):
+        for cha in range(img.shape[0]):
+            img[row,col,cha] = 255 - img[cha,col,row]
+'''
+img=255-img
+
+cv2.imshow('image inversé', img)
+cv2.waitkey()
